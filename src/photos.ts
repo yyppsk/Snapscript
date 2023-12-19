cb7f5d973415eafbe6617f0de7bbcfd6b77465f3
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-12-19 21:36:38 +0530

    first commit

---
+++ src/photos.ts
@@ -0,0 +1,243 @@
+export type Photo = {
+  // Photo ID (unique). It will also become the URL slug.
+  id: number | string;
+  // Photo title
+  title: string;
+  // Photo URL (can be absolute or relative to the public folder)
+  url: string;
+  // Photo description
+  description: string;
+};
+
+export const photos: Photo[] = [
+  {
+    id: 1,
+    title: "The promise of undying love",
+    description: "The promise of undying love. Featuring @shrutii_mishraaaa",
+    url: "/img/1.webp",
+  },
+  {
+    id: 2,
+    title: "The promise of undying love",
+    description: "The promise of undying love. Featuring @shrutii_mishraaaa",
+    url: "/img/2.webp",
+  },
+  {
+    id: 3,
+    title: "The promise of undying love",
+    description: "The promise of undying love. Featuring @shrutii_mishraaaa",
+    url: "/img/3.webp",
+  },
+  {
+    id: 4,
+    title: "Classical Show",
+    description:
+      "के फ़साना बन गई है, मेरी बात टलते टलते.  Featuring @shrutii_mishraaaa",
+    url: "/img/4.webp",
+  },
+  {
+    id: 5,
+    title: "Classical Show",
+    description:
+      "के फ़साना बन गई है, मेरी बात टलते टलते.  Featuring @shrutii_mishraaaa",
+    url: "/img/5.webp",
+  },
+  {
+    id: 6,
+    title: "Delusion",
+    description:
+      "Ek ladki hain anjani si fictional character pe vo marti hai. Featuring @___shruti_241",
+    url: "/img/6.webp",
+  },
+  {
+    id: 7,
+    title: "Delusion",
+    description:
+      "Ek ladki hain anjani si fictional character pe vo marti hai. Featuring @___shruti_241",
+    url: "/img/7.webp",
+  },
+  {
+    id: 8,
+    title: "Delusion",
+    description:
+      "Ek ladki hain anjani si fictional character pe vo marti hai. Featuring @___shruti_241",
+    url: "/img/8.webp",
+  },
+  {
+    id: 9,
+    title: "Aadhyaa khare",
+    description: "Country women attract heaven",
+    url: "/img/9.webp",
+  },
+  {
+    id: 10,
+    title: "Aadhyaa khare",
+    description: "Country women attract heaven",
+    url: "/img/10.webp",
+  },
+  {
+    id: 11,
+    title: "Dolly Sharma",
+    description:
+      "जहाँ भी आज़ाद रूह की झलक पड़े समझना वह मेरा घर है| - अमृता प्रीतम",
+    url: "/img/11.webp",
+  },
+  {
+    id: 12,
+    title: "Dolly Sharma",
+    description:
+      "जहाँ भी आज़ाद रूह की झलक पड़े समझना वह मेरा घर है| - अमृता प्रीतम",
+    url: "/img/12.webp",
+  },
+  {
+    id: 13,
+    title: "Dolly Sharma",
+    description:
+      "जहाँ भी आज़ाद रूह की झलक पड़े समझना वह मेरा घर है| - अमृता प्रीतम",
+    url: "/img/13.webp",
+  },
+  {
+    id: 14,
+    title: "Death by JavaScript",
+    description:
+      "Death by JavaScript: When you've been deprecated but don't know it yet.",
+    url: "/img/14.webp",
+  },
+  {
+    id: 15,
+    title: "Death by JavaScript",
+    description:
+      "How did the coder die? They ran out of heap memory while trying to store their dreams.",
+    url: "/img/15.webp",
+  },
+  {
+    id: 16,
+    title: "Death by JavaScript",
+    description:
+      "Why did the developer pass away? Because they tried to multi-thread their single-threaded life.",
+    url: "/img/16.webp",
+  },
+  {
+    id: 17,
+    title: "Death by JavaScript",
+    description:
+      "Death by JavaScript: Where you're in strict mode, but life isn't.",
+    url: "/img/17.webp",
+  },
+  {
+    id: 18,
+    title: "Death by JavaScript",
+    description:
+      "Why did the coder die? Because their git commit was rejected from the repository of life.",
+    url: "/img/18.webp",
+  },
+  {
+    id: 19,
+    title: "The Snapscript",
+    description: "I capture faces, emotions, bodies, places & feelings!",
+    url: "/img/19.webp",
+  },
+  {
+    id: 20,
+    title: "Wah Taj",
+    description:
+      "I still remember one of my favorite scenes from the film 'The Namesake when Tabu told Irrfan Khan, 'Just imagine how much Shahjahan must have loved Mumtaz to make this for her', Irrfan Khan said, 'Other husbands also love their wife, Ashima.. only we cannot afford to build Tajmahals', love is all anyone ever needs. Everything else is always filling the emptiness created by the lack of love!.",
+    url: "/img/20.webp",
+  },
+  {
+    id: 21,
+    title: "Wah Taj",
+    description:
+      "I still remember one of my favorite scenes from the film 'The Namesake when Tabu told Irrfan Khan, 'Just imagine how much Shahjahan must have loved Mumtaz to make this for her', Irrfan Khan said, 'Other husbands also love their wife, Ashima.. only we cannot afford to build Tajmahals', love is all anyone ever needs. Everything else is always filling the emptiness created by the lack of love!.",
+    url: "/img/21.webp",
+  },
+  {
+    id: 22,
+    title: "The Snapscript",
+    description: "I capture faces, emotions, bodies, places & feelings!",
+    url: "/img/22.webp",
+  },
+  {
+    id: 23,
+    title: "Incredible Jama Masjid",
+    description: "Different shades of Jama masjid✨",
+    url: "/img/23.webp",
+  },
+  {
+    id: 24,
+    title: "Incredible Jama Masjid",
+    description: "Different shades of Jama masjid✨",
+    url: "/img/24.webp",
+  },
+  {
+    id: 25,
+    title: "Incredible Jama Masjid",
+    description: "Different shades of Jama masjid✨",
+    url: "/img/25.webp",
+  },
+  {
+    id: 26,
+    title: "Yamuna Ghat Diaries",
+    description:
+      "How can we capture the essence of forever with a simple photograph?.",
+    url: "/img/26.webp",
+  },
+  {
+    id: 27,
+    title: "Yamuna Ghat Diaries",
+    description:
+      "How can we capture the essence of forever with a simple photograph?.",
+    url: "/img/27.webp",
+  },
+  {
+    id: 28,
+    title: "Landscape of my own land",
+    description:
+      "The curves, the patterns and the edge exist a little differently in each body, every fall and dip unique to itself.",
+    url: "/img/28.webp",
+  },
+  {
+    id: 29,
+    title: "The Snapscript",
+    description: "I capture faces, emotions, bodies, places & feelings!",
+    url: "/img/29.webp",
+  },
+  {
+    id: 30,
+    title: "Shades",
+    description: "Shades of yellow on cherry red",
+    url: "/img/30.webp",
+  },
+  {
+    id: 31,
+    title: "Blinding Lights",
+    description: "The night is more alive & colourful than the day.",
+    url: "/img/31.webp",
+  },
+  {
+    id: 32,
+    title: "Dolly Sharma",
+    description: "Love, Hope And Magic",
+    url: "/img/32.webp",
+  },
+  {
+    id: 33,
+    title: "Refraction",
+    description:
+      "This is basic phenomenon called refraction which is what happens when light passes through some medium nd changes it's direction because of it. All in our lives we've been told that we are reflection of our mistake but this picture talk about how we refract, we change and we grow. ",
+    url: "/img/33.webp",
+  },
+  {
+    id: 34,
+    title: "Landscape of my own land",
+    description:
+      "The curves, the patterns and the edge exist a little differently in each body, every fall and dip unique to itself.",
+    url: "/img/34.webp",
+  },
+  {
+    id: 35,
+    title: "Blinding Lights",
+    description: "The night is more alive & colourful than the day.",
+    url: "/img/35.webp",
+  },
+];
